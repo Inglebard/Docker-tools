8f3fd646c9adad4a7d29695a268600fc57e32dd4
Inglebard <dav87legrand@hotmail.fr>
2017-08-19 15:45:24 +0200

    Add files via upload

---
+++ Readme.md
@@ -0,0 +1,11 @@
+# Docker-tools
+Contains some bash/sh scripts for docker
+
+### backup-volumes-sh
+- Backup all volumes or selected volume(s)
+
+### backup-containers-volumes.sh
+- Backup all exposed volumes from all containers or selected container(s)
+
+### backup-containers-mounts.sh
+- Todo maybe
---
+++ backup-volumes.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+# This script allows you to backup all volumes from a container in a tar.gz file
+
+BACKUP_PATH="/home/david/Backup/"
+#DATE=$(date +"%d-%m-%Y_%H-%M-%S")
+DATE=$(date +"%A")
+
+if [ "$1" = 'all' ] || [ -z "$1" ]
+then
+    VOLUMES=($(docker volume ls -q))
+else
+    VOLUMES="$1"
+fi
+
+for ((i = 0; i < "${#VOLUMES[@]}"; i++))
+do
+    VOLUME_NAME="${VOLUMES[$i]}"
+	
+    FILENAME="${VOLUME_NAME}_${DATE}.tar.gz"
+    
+    echo "Backup docker's volume(s) : "$VOLUME_NAME
+    printf "\n"
+
+    CHECK_RUNNING=$(docker ps --filter "status=running" --filter "volume=$VOLUME_NAME" -q)
+
+    if [ ! -z "$CHECK_RUNNING" ]
+    then
+        echo "Pause docker container : "$CHECK_RUNNING
+        docker pause $CHECK_RUNNING >> /dev/null
+    fi
+
+    echo "Backup Volume from "$VOLUME_NAME" container"
+    docker run --rm -v $VOLUME_NAME:/tmp/$VOLUME_NAME -v "$BACKUP_PATH":/backup ubuntu tar -C "/tmp/" -P -czf "/backup/$FILENAME" "$VOLUME_NAME" >> /dev/null
+
+    if [ ! -z "$CHECK_RUNNING" ]
+    then
+        echo "Unpause docker container : "$CHECK_RUNNING
+        docker unpause $CHECK_RUNNING >> /dev/null
+    fi
+    
+    echo "Output file name : "$FILENAME
+
+    echo "Done."
+
+done
